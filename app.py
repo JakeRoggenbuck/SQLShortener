4ac167a1281bd81017ba2c51c2263c7ef9d8e383
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-08-10 13:26:21 -0700

    Add pages and change app, database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from database import DataBase
 
 app = Flask(__name__)
@@ -8,26 +8,34 @@ database.setup_db()
 
 @app.route('/')
 def index():
-    return "<h1>no page, cry</h1>"
+    return render_template('home.html')
 
 
 @app.route('/<string:tag>')
 def get(tag):
     data = database.tag_check(tag)
     if data != []:
-        return render_template('index.html', url=data[0][1])
+        return render_template('link.html', url=data[0][1])
     else:
-        return "<h1>no page, cry</h1>"
+        return render_template('404.html')
 
 
-@app.route('/new/<string:new>')
-def write(new):
-    new = new.split("!")
-    new_tag = database.write("https://" + new[0], new[1])
-    if new_tag:
-        return f"<h1>Yay! {new[0]} is now a page!</h1>"
+@app.route('/new/')
+def new_short():
+    return render_template('form.html')
+
+
+@app.route('/new/', methods=['POST'])
+def new_short_post():
+    alias = request.form['alias']
+    url = request.form['url']
+    if 'http' not in url:
+        url = 'https://' + url
+    write = database.write(url, alias)
+    if write:
+        return render_template('success.html', _alias=alias, _url=url)
     else:
-        return f"<h1>Yikes, {new[0]} is taken</h1>"
+        return render_template('error.html', _alias=alias)
 
 
 @app.route('/console/<string:data>')
@@ -39,7 +47,7 @@ def console(data):
         for links in all_links:
             page += field(links[2], links[1])
         return page
-    return "<p>¯\\_(ツ)_/¯ no data</p>"
+    return render_template('nodata.html')
 
 
 if __name__ == '__main__':
